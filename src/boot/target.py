c60c34b8017efeef98f045d434cba684c5776150
Jay Heng <jie.heng@nxp.com>
2018-10-31 14:12:28 +0800

    [BOOT] Cleanup target.py
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/boot/target.py
+++ src/boot/target.py
@@ -31,7 +31,7 @@
 import sys, os
 import commands, memoryrange, peripherals
 sys.path.append(os.path.abspath(".."))
-from utils import misc, elf
+from utils import misc
 
 ##
 # Bootloader target definition.
@@ -45,8 +45,6 @@ class Target(object):
         self.build = build
 
         self.baseDir = misc.get_dict_default(kwargs, 'baseDir', '.')
-        self.elfFilename = misc.get_dict_default(kwargs, 'elfFilename', None)
-        self.binFilename = misc.get_dict_default(kwargs, 'binFilename', None)
         self.memoryRange = misc.get_dict_default(kwargs, 'memoryRange', {})
         self.availableCommands = misc.get_dict_default(kwargs, 'availableCommands', 0)
         self.availablePeripherals = misc.get_dict_default(kwargs, 'availablePeripherals', 0)
@@ -55,38 +53,6 @@ class Target(object):
         self.flashloaderUsbVid = misc.get_dict_default(kwargs, 'flashloaderUsbVid', None)
         self.flashloaderUsbPid = misc.get_dict_default(kwargs, 'flashloaderUsbPid', None)
         self.supportedPeripheralSpeed_uart = misc.get_dict_default(kwargs, 'supportedPeripheralSpeed_uart', None)
-        self.testWorkingDir = misc.get_dict_default(kwargs, 'testWorkingDir', None)
-
-        self.elfObject = None
-        self.reservedRanges = []
-
-        # Get the target's flash memory region and flash sector size.
-        try:
-            self.flashMemory = self.memoryRange['flash']
-        except KeyError:
-            self.flashMemory = None
-            self.flashSectorSize = 1
-        else:
-            self.flashSectorSize = self.flashMemory.flashSectorSize
-        
-        # Get the target's ram memory region.
-        try:
-            self.ramMemory = self.memoryRange['ram']
-        except KeyError:
-            self.ramMemory = None
-        
-        # Read the elf file info and get reserved ranges.
-        if self.elfFilename is not None and os.path.exists(self.elfFilename):
-            with open(self.elfFilename, 'rb') as f:
-                e = elf.ELFObject()
-                e.fromFile(f)
-                self.elfObject = e
-        
-            self.reservedRanges = self._getReservedRanges()
-
-        self.freeFlashRange = self._getFreeRange(self.flashMemory)
-        self.freeRamRange = self._getFreeRange(self.ramMemory)
-
         
     ##
     # @brief Check if a command is supported by the target.
@@ -101,60 +67,6 @@ class Target(object):
     # @return True if the peripheral is supported. False if not.
     def isPeripheralSupported(self, name):
         return bool(peripherals.PeripheralMasks[name].propertyMask & self.availablePeripherals)
-    
-    ##
-    # @brief Determines the list of reserved memory ranges.
-    #
-    # @return A list of MemoryRange objects.
-    def _getReservedRanges(self):
-        if not self.elfObject:
-            return []
-        
-        # Only consider sections whose Write and Alloc flags are both set, and are
-        # non-zero in size.
-        writeAndAllocFlags = elf.ELFSection.SHF_WRITE | elf.ELFSection.SHF_ALLOC
-        alloced = [s for s in self.elfObject.sections if (bool(s.sh_flags & writeAndAllocFlags)) and (s.sh_size > 0)]
-        progbits = [s for s in alloced if s.sh_type == elf.ELFSection.SHT_PROGBITS]
-        nobits = [s for s in alloced if s.sh_type == elf.ELFSection.SHT_NOBITS]
 
-        ranges = []
-        for s in progbits + nobits:
-            r = memoryrange.MemoryRange(s.lma, s.sh_size)
-            
-            # If the reserved region is in flash, we have to sector align it.
-            if self.flashMemory is not None:
-                if self.flashMemory.contains(r):
-                    # Align start and end, recompute length.
-                    start = misc.align_down(r.start, self.flashSectorSize)
-                    end = misc.align_up(r.end, self.flashSectorSize)
-                    length = end - start
-                
-                    # Create aligned memory range.
-                    r = memoryrange.MemoryRange(start, length)
-            
-            # Only save this range if it is within one of the defined memory regions for
-            # this target.
-            for i in self.memoryRange.values():
-                ## @todo Should add only intersection to list
-                if i.intersects(r):
-                    ranges = memoryrange.addRangeToListAndCoalesce(ranges, r)
-                    break
-        
-        return ranges
-    
-    ##
-    # @brief Determine free memory range.
-    #
-    def _getFreeRange(self, fullRange):
-        if fullRange is not None:
-            # Check if requested range is contained in any reserved range
-            for r in self.reservedRanges:
-                if fullRange.contains(r):
-                    # Return a range that starts after the end of the reserved range and goes to the end of the full range
-                    start = r.end + 1
-                    len = fullRange.end + 1 - start
-                    return memoryrange.MemoryRange(start, len)
-        # Requested range not contained in any reserved range, so full range is free
-        return fullRange
 
         
--- src/targets/MIMXRT1052/bltargetconfig.py
+++ src/targets/MIMXRT1052/bltargetconfig.py
@@ -44,64 +44,18 @@ flashloaderUsbVid = '0x15A2'
 flashloaderUsbPid = '0x0073'
 availableCommands = 0x5EFDF
 supportedPeripheralSpeed_uart = [4800, 9600, 19200, 57600, 115200] # @todo Verify
-elfFilename = os.path.join(os.path.dirname(__file__), compiler, board, 'output', build, board + '.elf')
-binFilename = os.path.join(os.path.dirname(__file__), compiler, board, 'output', build, board + '.bin')
-testWorkingDir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'test', 'working')
 
 # memory map
 memoryRange = {
-    # ITCM, 256KByte(64KByte)
-    'itcm' : MemoryRange(0x00000000, 0x10000, 0x00040000, 'state_mem0.dat'),
-    # DTCM, 256KByte(64KByte)
-    'dtcm' : MemoryRange(0x20000000, 0x10000, 0x20040000, 'state_mem1.dat'),
-    # OCRAM, 256KByte(128KByte)
-    'ram' : MemoryRange(0x20200000, 0x20000, 0x20240000, 'state_mem2.dat'),
-
-    # SEMC Parallel NOR (Micron MT28EW128ABA1LPC-0SIT)
-    # Micron MT28EW128ABA1LPC-0SIT    (x8/x16 bits, 32B Page/128KB Block/128Mb Device,   Non-ADM, Asynchronous)
-    # Winbond W29GL128CH9T            (x8/x16 bits, 64B Page/128KB Sector/128Mb Device,  Non-ADM, Asynchronous)
-    # Spansion S29GL128S90TFI020      (x16 bits,    512B Page/128KB Sector/128Mb Device, Non-ADM, Asynchronous)
-    'SEMC_Parallel_NOR' : MemoryRange(0x90000000, 0x01000000, 0x91000000, 'state_ex_Paralle_NOR.dat', True, True, 0x20, 0x20000),
-
-    # Serial NOR over FlexSPI module (IS25LP064A-JBLE) (8MB)
-    'FlexSPI_Serial_NOR' : MemoryRange(0x60000000, 0x00800000, 0x60800000, 'state_ex_Paralle_NOR.dat', True, True, 0x100, 0x10000),
-
-    # SEMC Raw NAND (Micron MT29F16G08ABACAWP:C) 0x0 - 0x80000000 2GB
-    # Macronix MX30LF4GE8AB-TI        (x8 bits, 2KB Page/128KB Block/4Gb Device,  0bit ECC, 3.3V)
-    # Micron MT29F4G08ABBDAWP         (x8 bits, 2KB Page/128KB Block/4Gb Device,  4bit ECC, 1.8V)
-    # Micron MT29F4G08ABAFAWP:D       (x8 bits, 2KB Page/128KB Block/4Gb Device,  4bit ECC, 3.3V)
-    # Micron MT29F16G08ABACAWP:C      (x8 bits, 4KB Page/512KB Block/16Gb Device, 4bit ECC, 3.3V)
-    # Winbond W29N01GVSIAA            (x8 bits, 2KB Page/128KB Block/1Gb Device,  1bit ECC, 3.3V)
-    'SEMC_Raw_NAND' : MemoryRange(0x100000, 0x0FF00000, 0x0FF00000, 'state_ex_raw_NAND.dat', True, False, 0x1000, 0x80000),
-
-    # LPSPI Serial NOR/EEPROM, 128KB (Micron MT25QL128ABA1ESE-OSIT)
-    # Onsemi CAT25512HU5I-GT3         (EEPROM,    1-bit SPI,    20MHz,      128B Page/512Kb Device)
-    # Micron MT25QL128ABA1ESE-OSIT    (NOR Flash, Multiple I/O, 133MHz-STR, 64B Page/4-32-64KB Sector/128Mb Device)
-    # Spansion S25FL129P              (NOR Flash, Multiple I/O, 80MHz,      256B Page/4-8-64-256KB Sector/128Mb Device)
-    'LPSPI_Serial_NOR_EEPROM' : MemoryRange(0x0, 0x01000000, 0x01000000, 'state_ex_ser_nor_eeprom.dat', True, False, 0x40, 0x1000),
-
-    # uSDHC Managed NAND SD
-    'uSDHC_Managed_NAND_SD' : MemoryRange(0x00000000, 0x01000000, 0x01000000, 'state_ex_Paralle_NOR.dat', True, False, 0x200, 0x20000),
-
-    # uSDHC Managed NAND eMMC (MTFC8GAKAJCN-1M WT)
-    'uSDHC_Managed_NAND_eMMC' : MemoryRange(0x00000000, 0x08000000, 0x08000000, 'state_ex_Paralle_NOR.dat', True, False, 0x200, 0x20000)
+    # ITCM, 256KByte
+    'itcm' : MemoryRange(0x00000000, 0x40000, 'state_mem0.dat'),
+    # DTCM, 256KByte
+    'dtcm' : MemoryRange(0x20000000, 0x40000, 'state_mem1.dat'),
+    # OCRAM, 512KByte
+    'ram' : MemoryRange(0x20200000, 0x80000, 'state_mem2.dat')
 }
 
-reservedRegionDict = {   # new
-    # DTCM, 256KB
-    'dtcm' : [0x20000000, 0x2000DA77],
-    # OCRAM, 256KB
-    'ram' : [0x2020A000, 0x2021E6CC]
-}
-
-externalMemoryIdDict = {
-    0x00 : None,
-    0x01 : 'QuadSPI_Serial_NOR',
-    0x08 : 'SEMC_Parallel_NOR',
-    0x09 : 'FlexSPI_Serial_NOR',
-    0x100 : 'SEMC_Raw_NAND',
-    0x101 : 'FlexSPI_Serial_NAND',
-    0x110 : 'LPSPI_Serial_NOR_EEPROM',
-    0x120 : 'uSDHC_Managed_NAND_SD',
-    0x121 : 'uSDHC_Managed_NAND_eMMC'
+reservedRegionDict = {
+    # OCRAM, 512KB
+    'ram' : [0x20203800, 0x20207EF8]
 }
